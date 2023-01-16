c08895cefae5387330a7168edf106827735020f0
joacasallas <joacasallas@gmail.com>
2023-01-16 15:01:49 -0500

    task 1. Embedding expressions, functions

--- 0x02-react_intro/task_1/dashboard/src/App.js
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.jpg';
 import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       </div>
       <div className="App-footer">
         <p>
-          Copyright 2020 - holberton School
+          Copyright {getFullYear()} - {getFooterCopy(true)}
         </p>
       </div>
     </div>
