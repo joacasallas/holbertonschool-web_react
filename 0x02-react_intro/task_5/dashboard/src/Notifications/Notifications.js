76fa8eac4b5fcd1f65d56ce9427edc3d5290dad4
joacasallas <joacasallas@gmail.com>
2023-01-26 16:59:52 -0500

    8. Create a project using Webpack

---
+++ 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from '..assets/closeIcon.png';
+import {getLatestNotification} from '..utils/utils'
+
+
+function Notifications() {
+    function handleClick() {
+        console.log("Close button has been clicked")
+    }
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent">{getLatestNotification()}</li>
+            </ul>
+            <button onClick={handleClick} aria-label="Close" style={{
+                background:'transparent',
+                border:'none',
+                display: 'inline',
+                right: 20,
+                top:20,
+                position: 'absolute',
+            }}>
+                <img src={closeIcon} className="closeIcon" alt="closeIcon" style={{
+                height:10
+            }}/>
+            </button>
+        </div>
+    );
+};
+
+export default Notifications;
