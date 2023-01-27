5e7865f248c1f8af1d8c780fbce03170c0cc6f19
joacasallas <joacasallas@gmail.com>
2023-01-27 06:13:21 -0500

    0x01-Webpack

---
+++ 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,20 @@
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+
+describe('testing <Notifications/>', ()=>{
+    let wrapper = shallow(<Notifications />);
+    beforeEach(()=>{
+        wrapper = shallow(<Notifications />);
+    });
+
+    test('it renders without crashing', ()=>{
+        expect(wrapper.exists()).toEqual(true);
+    });
+    test('it renders three list items', ()=>{
+        expect(wrapper.find("li")).toHaveLength(3);
+    });
+    test('it renders the const text ', ()=>{
+        const text = "Here is the list of notifications";
+        expect(wrapper.find(".Notifications p").text()).toEqual(text);
+    });
+})
